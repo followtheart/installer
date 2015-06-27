b71c08584b8c624c77d8f7251334f889e9f206e5
followtheart@outlook.com <followtheart@outlook.com>
2015-06-27 21:24:42 +0000

    update laravel 添加常用php组件

--- laravel.sh
+++ laravel.sh
@@ -2,6 +2,9 @@
 sh composer.sh
 composer global require "laravel/installer=~1.1"
 
+#添加laravel常用的php组件
+sudo apt-get install php5-curl php5-mcrypt php5-gd
+sudo service apache2 restart
 
 echo 'export PATH=$PATH:"~/.composer/vendor/bin/"' >> /etc/profile
 
