c5bcb697faf82a5b2222be935d55d98b839a3faf
followtheart <followtheart@outlook.com>
2015-07-06 05:29:09 +0800

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -1,5 +1,11 @@
 #安装composer
 #https://github.com/composer/composer
+
+#安装php5
 sudo apt-get install php5
+
+#安装
 curl -sS https://getcomposer.org/installer | php
+
+#移动到可执行目录
 mv composer.phar /usr/local/bin/composer
