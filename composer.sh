d31127f81ce1a73299188772a5fdce396f0e561b
followtheart <followtheart@outlook.com>
2015-08-04 10:19:41 +0800

    add some echo

--- composer.sh
+++ composer.sh
@@ -2,10 +2,13 @@
 #https://github.com/composer/composer
 
 #安装php5
+echo "Installing php5 and php5-cli..."
 sudo apt-get install php5 php5-cli -y
 
 #安装
+echo "Installing composer..."
 curl -sS https://getcomposer.org/installer | php
 
 #移动到可执行目录
-mv composer.phar /usr/local/bin/composer
+echo "mv composer.phar /usr/local/bin/composer ..."
+sudo mv composer.phar /usr/local/bin/composer
--- laravel.sh
+++ laravel.sh
@@ -1,11 +1,14 @@
 #laravel 配置
 ##第一步安装composer
+echo "laravel.sh-installing composer"
 curl -sS https://raw.githubusercontent.com/followtheart/installer/master/composer.sh | sh
 
 ##第二步安装laravel-framework
+echo "laravel.sh-composer global require \"laravel/installer=~1.1\""
 composer global require "laravel/installer=~1.1"
 
 #添加laravel常用的php组件
+echo "laravel.sh-install php5-curl php5-mcrypt php5-gd..."
 sudo apt-get install php5-curl php5-mcrypt php5-gd
 sudo service apache2 restart
 
