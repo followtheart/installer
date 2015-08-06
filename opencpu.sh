5aa8b1404a361f299535f925b1655bdec845a896
followtheart <followtheart@outlook.com>
2015-08-06 23:54:43 +0800

    Update opencpu.sh

--- opencpu.sh
+++ opencpu.sh
@@ -1,5 +1,5 @@
 #requires ubuntu 14.04 (Trusty)
 sudo add-apt-repository ppa:opencpu/opencpu-1.4
 sudo apt-get update
-sudo apt-get install opencpu
+sudo apt-get install opencpu -y
 sudo apt-get install rstudio-server #optional
