8b06e4c5bb89acaf4ec94f9aad4ea4946e403250
followtheart <followtheart@outlook.com>
2015-11-19 20:02:23 +0800

    bitcoin

---
+++ coins/bitcoin.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-4.9 g++-4.9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
