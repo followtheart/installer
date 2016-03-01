c57f55140a0f4171ea6b049c44a4599af3a4802b
followtheart <followtheart@outlook.com>
2016-03-02 05:23:47 +0800

    Update bitcoin.sh

--- coins/bitcoin.sh
+++ coins/bitcoin.sh
@@ -4,10 +4,10 @@ sudo apt-get update
 
 sudo apt-get install libdb4.8-dev libdb4.8++-dev git libssl-dev -y 
 apt-get install dos2unix -y
-apt-get install libboost-all-dev
+apt-get install libboost-all-dev -y
 apt-get install libminiupnpc-dev pkg-config autotools-dev autoconf automake libtool -y
 
 
 
 #cross compile for windows
-sudo apt-get install g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 mingw-w64-x86-64-dev
+sudo apt-get install g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 mingw-w64-x86-64-dev -y
