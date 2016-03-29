c599fe30b05111bc7947f215ef8947faef0b03fd
followtheart <followtheart@outlook.com>
2016-03-29 15:06:45 +0800

    Update bitcoin.sh

--- coins/bitcoin.sh
+++ coins/bitcoin.sh
@@ -4,7 +4,7 @@ sudo apt-get install software-properties-common -y
 add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 
-sudo apt-get install libdb4.8-dev libdb4.8++-dev git libssl-dev -y 
+sudo apt-get install libdb4.8-dev libdb4.8++-dev git libssl-dev libcurl4-openssl-dev -y 
 apt-get install dos2unix -y
 apt-get install libboost-all-dev -y
 apt-get install libminiupnpc-dev pkg-config autotools-dev autoconf automake libtool -y
