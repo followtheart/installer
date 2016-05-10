f282a6014f377973e551826423e542f4766480b0
followtheart <followtheart@outlook.com>
2016-05-11 00:09:57 +0800

    Update bitcoin.sh

--- coins/bitcoin.sh
+++ coins/bitcoin.sh
@@ -1,12 +1,9 @@
 apt-get update
-apt-get install curl git  libstdc++6 -y
-sudo apt-get install software-properties-common imagemagick -y 
+apt-get install curl git  libstdc++6 software-properties-common imagemagick -y 
 add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
- sudo apt-get update;
- sudo apt-get install gcc-4.8 g++-4.8
-sudo apt-get install libdb4.8-dev libdb4.8++-dev git libssl-dev libcurl4-openssl-dev libssl-dev -y 
+sudo apt-get update
+sudo apt-get install gcc-4.8 g++-4.8 libdb4.8-dev libdb4.8++-dev git libssl-dev libcurl4-openssl-dev libssl-dev -y 
 apt-get install dos2unix -y
 apt-get install libboost1.55-all-dev -y
 apt-get install libminiupnpc-dev pkg-config autotools-dev autoconf automake libtool -y
