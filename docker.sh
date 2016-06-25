8a14989a993fb938f70a72fa5963c2d57093e859
followtheart <followtheart@outlook.com>
2016-06-25 11:17:28 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,8 +9,15 @@
 #
 #By followtheart
 sudo apt-get update
-sudo apt-get install wget
+ sudo apt-get install apt-transport-https ca-certificates
+ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+ deb https://apt.dockerproject.org/repo ubuntu-trusty main
+ sudo apt-get purge lxc-docker
+  apt-cache policy docker-engine
+  apt-get upgrade
+  
+#sudo apt-get install wget
 
-wget -qO- https://get.docker.com/ | sh
+#wget -qO- https://get.docker.com/ | sh
 
 docker run hello-world
