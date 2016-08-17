42b489e3604a80021dda2fb4df4e6382f3097b6e
followtheart <followtheart@outlook.com>
2016-08-17 17:30:09 +0800

    Update mongdb.sh
    使用阿里云的源

--- mongdb.sh
+++ mongdb.sh
@@ -1,8 +1,8 @@
 #Step 1:  Import the MongoDB public key 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
 
 #Step 2: Generate a file with the MongoDB repository url
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+echo 'deb http://mirrors.aliyun.com/mongodb/apt/ubuntu trusty/mongodb-org/stable multiverse' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
 #Step 3: Refresh the local database with the packages
 sudo apt-get update
