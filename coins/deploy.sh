95fa4f7053e618e8a7312cbee57e837e4c4c57ed
thelastwolf <aapool@outlook.com>
2016-08-30 17:39:19 +0800

    update deploy

--- coins/deploy.sh
+++ coins/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 ssh-keygen
-apt-get install libboost-system1.55-dev libboost-filesystem1.55-dev libboost-thread1.55-dev libboost-program-options1.55-dev
-apt-get install software-properties-common
-apt-get install language-pack-zh-hans
+apt-get install libboost-system1.55-dev libboost-filesystem1.55-dev libboost-thread1.55-dev libboost-program-options1.55-dev -y
+apt-get install software-properties-common -y
+apt-get install language-pack-zh-hans -y
 add-apt-repository ppa:bitcoin/bitcoin
 apt-get install software-properties-common
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install libdb4.8++-dev
-apt-get install libminiupnpc-dev
-
+apt-get install libdb4.8++-dev -y
+apt-get install libminiupnpc-dev -y
