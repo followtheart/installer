5765ba2c9b281e4c2a39170088384537bbc0e989
thelastwolf <aapool@outlook.com>
2016-08-31 14:57:50 +0800

    update deploy

--- coins/deploy.sh
+++ coins/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 ssh-keygen
+apt-get update
 apt-get install libboost-system1.55-dev libboost-filesystem1.55-dev libboost-thread1.55-dev libboost-program-options1.55-dev -y
 apt-get install software-properties-common -y
 apt-get install language-pack-zh-hans -y
 add-apt-repository ppa:bitcoin/bitcoin
-apt-get install software-properties-common
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
 apt-get install libdb4.8++-dev -y
