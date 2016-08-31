d163bd652855147b36d621a601ddd9deeb8a5733
thelastwolf <aapool@outlook.com>
2016-08-31 16:01:49 +0800

    update node12.sh

--- nodejs0.12.sh
+++ nodejs0.12.sh
@@ -1,4 +1,9 @@
 #参考链接
 #https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-sudo apt-get install -y nodejs
+cd /bin/
+wget https://npm.taobao.org/mirrors/node/latest-v0.12.x/node-v0.12.15-linux-x64.tar.gz
+tar xvzf node-v0.12.15-linux-x64.tar.gz
+ln -s node-v0.12.15-linux-x64/bin/node node
+ln -s node-v0.12.15-linux-x64/lib/node_modules/npm/bin/npm-cli.js npm
+
+exit
