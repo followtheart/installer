6f65657549e32143d63076d6a090a6bb45b193ed
followtheart <followtheart@outlook.com>
2016-10-09 17:05:50 +0800

    Update deploy.sh

--- coins/deploy.sh
+++ coins/deploy.sh
@@ -2,7 +2,7 @@
 ssh-keygen
 apt-get update
 apt-get install libboost-system1.55-dev libboost-filesystem1.55-dev libboost-thread1.55-dev libboost-program-options1.55-dev -y
-apt-get install software-properties-common -y
+apt-get install software-properties-common zip -y
 apt-get install language-pack-zh-hans -y
 add-apt-repository ppa:bitcoin/bitcoin
 add-apt-repository ppa:ubuntu-toolchain-r/test
