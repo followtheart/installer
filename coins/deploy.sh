3e80e5a51824984eb3b948858488f309a42ee995
thelastwolf <aapool@outlook.com>
2016-10-25 10:03:54 +0800

    add git

--- coins/deploy.sh
+++ coins/deploy.sh
@@ -7,5 +7,5 @@ apt-get install language-pack-zh-hans -y
 add-apt-repository ppa:bitcoin/bitcoin
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install libdb4.8++-dev zip -y
+apt-get install libdb4.8++-dev zip git -y
 apt-get install libminiupnpc-dev -y
